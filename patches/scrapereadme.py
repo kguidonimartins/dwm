b9b5630a10b332c8db27dcaf88629b1b14855cc2
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-11-08 12:23:37 -0300

    scrape readme

---
+++ patches/scrapereadme.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Scrape readme."""
+
+import os
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+patch_names = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+
+for idn, patch in enumerate(patch_names):
+    URL = "https://dwm.suckless.org/patches/" + patch
+    page = requests.get(URL)
+    if page.status_code == 404:
+        print(f"{URL} does not exists!")
+        continue
+    soup = BeautifulSoup(page.content, "html.parser")
+    readme = soup.find(id="main").text
+    readme_file = patch + "/README-site"
+    if not Path(readme_file).exists():
+        with open(readme_file, "w") as file:
+            file.write(readme)
