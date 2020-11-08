567eec242361d6307f3aa61bc0e254052c4a1667
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-11-08 16:55:45 -0300

    update scrap

--- patches/scrapereadme.py
+++ patches/scrapereadme.py
@@ -10,14 +10,14 @@ from bs4 import BeautifulSoup
 patch_names = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
 
 for idn, patch in enumerate(patch_names):
-    URL = "https://dwm.suckless.org/patches/" + patch
-    page = requests.get(URL)
-    if page.status_code == 404:
-        print(f"{URL} does not exists!")
-        continue
-    soup = BeautifulSoup(page.content, "html.parser")
-    readme = soup.find(id="main").text
     readme_file = patch + "/README-site"
     if not Path(readme_file).exists():
+        URL = "https://dwm.suckless.org/patches/" + patch
+        page = requests.get(URL)
+        if page.status_code == 404:
+            print(f"{URL} does not exists!")
+            continue
+        soup = BeautifulSoup(page.content, "html.parser")
+        readme = soup.find(id="main").text
         with open(readme_file, "w") as file:
             file.write(readme)
