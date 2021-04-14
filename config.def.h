ea37b4a392522f1edf2e07e0ea6d415c6d8c43ce
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-04-14 18:00:47 -0300

    add binding for slock

--- config.def.h
+++ config.def.h
@@ -226,7 +226,7 @@ static Key keys[] = {
     /* { MODKEY|ShiftMask,      XK_Return,          togglescratch,  {.ui = 0} }, */
 
     { MODKEY,               XK_z,               incrgaps,       {.i = +3 } },
-    { MODKEY,               XK_x,               incrgaps,       {.i = -3 } },
+    { MODKEY,               XK_x,               spawn,          SHCMD("slock") },
     { MODKEY|ShiftMask,     XK_c,               spawn,          SHCMD("camtoggle") },
     { MODKEY,               XK_b,               togglebar,      {0} },
     { MODKEY,               XK_v,               spawn,          SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
