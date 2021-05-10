40670c6d787a26ec1f7a369b7772a52378fd4c74
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-05-10 07:26:15 -0300

    Revert "show systray"
    This reverts commit e81095ceb49751e3477f7c94ba97feaf4a024863.

--- config.def.h
+++ config.def.h
@@ -5,22 +5,25 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx       = 0;        /* border pixel of windows */
-static const unsigned int snap           = 10;       /* snap pixel */
-static const unsigned int gappih         = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv         = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh         = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps               = 0;        /* 1 means no outer gap when there is only one window */
-static const int swallowfloating         = 1;        /* 1 means swallow floating windows by default */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static int showsystray                   = 1;     /* 0 means no systray */
-static const int showbar                 = 1;        /* 0 means no bar */
-static const int topbar                  = 1;        /* 0 means bottom bar */
-static const char *fonts[]               = { "UbuntuMono Nerd Font:size=10:antialias=true:autohint=12", "Font Awesome 5:size=8:antialias=true:autohint=true" };
-static char dmenufont[]                  = "UbuntuMono Nerd Font:size=8:antialias=true:autohint=8";
+static int showsystray        = 0;     /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = {
+                                        "UbuntuMono Nerd Font:size=10:antialias=true:autohint=12",
+                                        "Font Awesome 5:size=8:antialias=true:autohint=true"
+                                      };
+static char dmenufont[]             = "UbuntuMono Nerd Font:size=8:antialias=true:autohint=8";
 
 /* /1* DEFAULT COLORS *1/ */
 /* static const char col_gray1[]       = "#222222"; */
